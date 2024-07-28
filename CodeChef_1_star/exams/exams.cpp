a5568240408e86accd469550f5e8aa503edf97ab
Arshvir <avarshvir@gmail.com>
2024-07-28 22:11:41 +0530

    commit

---
+++ CodeChef_1_star/exams/exams.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X,Y,Z;
+	    cin >> X >> Y >> Z;
+	    if(Z > (X*Y)/2){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
