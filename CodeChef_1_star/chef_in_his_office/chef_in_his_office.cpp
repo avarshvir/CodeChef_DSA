d0fc18d083158c3b0a5f07dc3a663d873bf655e7
Arshvir <avarshvir@gmail.com>
2024-07-29 15:23:40 +0530

    Update chef_in_his_office.cpp

--- CodeChef_1_star/chef_in_his_office/chef_in_his_office.cpp
+++ CodeChef_1_star/chef_in_his_office/chef_in_his_office.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-
-
+    int T;
+    cin >> T;
+    while(T--){
+        int X,Y;
+        cin >> X >> Y;
+        cout << 4*X + Y << endl;
+    }
+    return 0;
 }
