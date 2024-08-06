80c517ad04c9951ba3e48d2937cd75b8c0955996
Arshvir <avarshvir@gmail.com>
2024-08-06 19:37:18 +0530

    commit

--- CodeChef_1_star/Minimum_Pizzas/Minimum_Pizzas.cpp
+++ CodeChef_1_star/Minimum_Pizzas/Minimum_Pizzas.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int main() {
+	// your code goes here
 	int T;
 	cin >> T;
 	while(T--){
 	    int N, X;
 	    cin >> N >> X;
-	    int pizzas_req = N*X;
+	    int total_slice = N*X;
+	    int total_pizzas = (total_slice+3)/4;
+	    cout << total_pizzas << endl;
 	    
 	}
 
