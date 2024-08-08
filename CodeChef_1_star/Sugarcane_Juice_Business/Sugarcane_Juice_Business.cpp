a2ef25605e7fa362ab984e2b5b6c2672a4899f75
Arshvir <avarshvir@gmail.com>
2024-08-08 19:15:58 +0530

    commit

---
+++ CodeChef_1_star/Sugarcane_Juice_Business/Sugarcane_Juice_Business.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    int totalIncome = 50 * N;  
+        int profit = 0.3 * totalIncome;  // 30% of total income is the profit
+        cout << profit << endl; 
+	    
+	}
+
+}
