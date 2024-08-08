4b86b587184a7f6c6ffee8ab5007c5fab06bd329
Arshvir <avarshvir@gmail.com>
2024-08-08 19:27:19 +0530

    Create Count_the_Notebooks.cpp

---
+++ CodeChef_1_star/Count_the_Notebooks/Count_the_Notebooks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    int total_notebooks = (N*1000)/100;
+	    cout << total_notebooks << endl;
+	}
+}
