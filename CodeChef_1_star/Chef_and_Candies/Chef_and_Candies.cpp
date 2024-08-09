646578add1e966bd46d2df6b85d82dbdc0fe060f
Arshvir <avarshvir@gmail.com>
2024-08-09 19:27:16 +0530

    commit

--- CodeChef_1_star/Chef_and_Candies/Chef_and_Candies.cpp
+++ CodeChef_1_star/Chef_and_Candies/Chef_and_Candies.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int N, X;
+        cin >> N >> X;
+        int temp; 
+        /*
+        temp is the number of additional candies Chef needs to buy in order to have enough candies to distribute one to each of the N children
+        */
+        if (N > X) {
+            temp = N - X;
+        } else {
+            temp = 0;
+        }
+        
+        int packets_needed = (temp + 3) / 4;
+        cout << packets_needed << endl;
+	}
 
 }
