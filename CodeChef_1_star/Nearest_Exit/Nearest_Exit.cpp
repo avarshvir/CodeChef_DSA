c64e10baf91321ffd408ab8ea80ca13a51d4addf
Arshvir <avarshvir@gmail.com>
2024-08-10 23:50:23 +0530

    Create Nearest_Exit.cpp

---
+++ CodeChef_1_star/Nearest_Exit/Nearest_Exit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X;
+	    cin >> X;
+	    if(X <= 100 && X >= 51){
+	        cout << "RIGHT" << endl;
+	    }
+	    else{
+	        cout << "LEFT" << endl;
+	    }
+	}
+
+}
