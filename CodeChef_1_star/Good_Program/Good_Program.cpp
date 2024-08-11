31f35b00a1b9fe9c84dda6d51ea1cf8c4c3b4ddd
Arshvir <avarshvir@gmail.com>
2024-08-11 21:25:09 +0530

    Create Good_Program.cpp

---
+++ CodeChef_1_star/Good_Program/Good_Program.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    if(N%4==0){
+	        cout << "Good" << endl;
+	    }
+	    else{
+	        cout << "Not Good" << endl;
+	    }
+	}
+
+}
