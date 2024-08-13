4a37183125ce028f0bf1b510ee73c3076440d58a
Arshvir <avarshvir@gmail.com>
2024-08-13 19:30:12 +0530

    Create Jenga_Night.cpp

---
+++ CodeChef_1_star/Jenga_Night/Jenga_Night.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N, X;
+	    cin >> N >> X;
+	    
+	    if(X%N==0){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
