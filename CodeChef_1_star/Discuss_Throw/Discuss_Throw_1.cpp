738dca16094502435ecb3f5eb011b7e1f5f70cdb
Arshvir <avarshvir@gmail.com>
2024-08-14 22:21:42 +0530

    Create Discuss_Throw_1.cpp

---
+++ CodeChef_1_star/Discuss_Throw/Discuss_Throw_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int A,B,C;
+	    cin >> A >> B >> C;
+	    cout << max({A,B,C}) << endl;
+	}
+
+}
