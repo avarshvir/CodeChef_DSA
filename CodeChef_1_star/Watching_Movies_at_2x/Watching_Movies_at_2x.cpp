2686ff4b14ff5c728b0262153d0331e544f5803a
Arshvir <avarshvir@gmail.com>
2024-08-16 20:53:31 +0530

    Create Watching_Movies_at_2x.cpp

---
+++ CodeChef_1_star/Watching_Movies_at_2x/Watching_Movies_at_2x.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X,Y;
+	cin >> X >> Y;
+	//int total = X+Y;
+	int twice_2X = Y/2;
+	int remaining_time = X-Y;
+	cout << twice_2X + remaining_time << endl;
+
+}
