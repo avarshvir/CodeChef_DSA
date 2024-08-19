a71b766223231af8ad65c70bb9add694fd030c21
Arshvir <avarshvir@gmail.com>
2024-08-19 18:13:02 +0530

    Update Mario_and_Bullet.cpp

--- CodeChef_1_star/Mario_and_Bullet/Mario_and_Bullet.cpp
+++ CodeChef_1_star/Mario_and_Bullet/Mario_and_Bullet.cpp
@@ -5,7 +5,16 @@ int main() {
 	int T;
 	cin >> T; 
 	while(T--){
+	    int X,Y,Z;
+	    cin >> X >> Y >> Z;
 	    
+	    int time_to_hit = Y/X;
+	    
+	    int wait_time = Z - time_to_hit;
+
+        wait_time = max(0, wait_time);
+
+        cout << wait_time << endl;
 	}
 
 }
