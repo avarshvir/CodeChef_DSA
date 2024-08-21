180358f18b5714f2ebeadb03003c39544ecff428
Arshvir <avarshvir@gmail.com>
2024-08-21 23:43:05 +0530

    Update Chef_and_Water_Bottles.cpp

---
+++ CodeChef_1_star/Chef_and_Water_Bottles/Chef_and_Water_Bottles.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+        int N,X,K;
+        cin >> N >> X >> K;
+        int total_bottle_filled = ()
+    }
+}
