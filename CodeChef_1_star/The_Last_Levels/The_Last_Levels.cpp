26116d38528cd13b5230d39a654232ccafde5429
Arshvir <avarshvir@gmail.com>
2024-08-23 19:47:34 +0530

    Update The_Last_Levels.cpp

--- CodeChef_1_star/The_Last_Levels/The_Last_Levels.cpp
+++ CodeChef_1_star/The_Last_Levels/The_Last_Levels.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+        
+        int playTime = X * Y;
+        
+        int numberOfBreaks = (X - 1) / 3; 
+        int breakTime = numberOfBreaks * Z;
+        
+        int totalTime = playTime + breakTime;
+        
+        cout << totalTime << endl;
+    }
+
 
 }
