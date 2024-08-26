a88c30ee1f5c8f77fff498a7f7f1ae2c3d29da09
Arshvir <avarshvir@gmail.com>
2024-08-26 15:19:54 +0530

    Update Blackjack.cpp

--- CodeChef_1_star/Blackjack/Blackjack.cpp
+++ CodeChef_1_star/Blackjack/Blackjack.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-
+	int T;
+	cin >> T;
+	while(T--){
+		int A,B,C;
+		cin >> A >> B;
+		C = 21-(A+B);
+		if(C >= 1 && C <=10){
+			cout << C << endl;
+		}
+		else{
+			cout << -1 << endl;
+		}
+	}
 }
