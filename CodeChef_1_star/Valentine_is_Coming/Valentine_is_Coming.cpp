3add6a626ab619d8b03c63bd6a41820eb91c9225
Arshvir <avarshvir@gmail.com>
2024-08-27 18:41:46 +0530

    Create Valentine_is_Coming.cpp

---
+++ CodeChef_1_star/Valentine_is_Coming/Valentine_is_Coming.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+        int X,Y;
+        cin >> X >> Y;
+        int total_chocolate = X/Y;
+        cout << total_chocolate << endl;
+    }
+}
