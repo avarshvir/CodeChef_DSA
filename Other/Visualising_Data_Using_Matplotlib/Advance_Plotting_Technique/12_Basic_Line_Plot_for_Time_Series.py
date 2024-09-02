be911639056a9cbef685cdb59dc6f6f9c20900a6
Arshvir <avarshvir@gmail.com>
2024-09-02 18:02:19 +0530

    commit

---
+++ Other/Visualising_Data_Using_Matplotlib/Advance_Plotting_Technique/12_Basic_Line_Plot_for_Time_Series.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create sample data
+data = {
+    'Date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05'],
+    'Sales': [100, 120, 110, 140, 130]
+}
+
+# Create the DataFrame
+df = pd.DataFrame(data)
+
+# Convert 'Date' to datetime and set as index
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
