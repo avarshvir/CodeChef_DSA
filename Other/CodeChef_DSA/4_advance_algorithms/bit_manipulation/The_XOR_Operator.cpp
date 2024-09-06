0c422a07ed6d0a944525c0d0d71a6cbd5f1dfc05
Arshvir <avarshvir@gmail.com>
2024-09-06 18:36:19 +0530

    commit

--- Other/CodeChef_DSA/4_advance_algorithms/bit_manipulation/The_XOR_Operator.cpp
+++ Other/CodeChef_DSA/4_advance_algorithms/bit_manipulation/The_XOR_Operator.cpp
@@ -6,7 +6,7 @@ int main() {
     A = 5; // 5 = 101 in Binary
     B = 11; // 11 = 1011 in Binary
 
-    int XOR = A & B; // 0101 ^ 1011
+    int XOR = A ^ B; // 0101 ^ 1011
 
     cout << "The Bitwise XOR is: " << XOR << "\n";
     
