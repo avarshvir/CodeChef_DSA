e247ce95a4e536e80965a9704be4f4c3e7ab2e5d
Arshvir <avarshvir@gmail.com>
2024-09-07 17:29:15 +0530

    commit

---
+++ Other/CodeChef_DSA/4_advance_algorithms/bit_manipulation/Signed_and_Unsigned_Datatypes.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int X = 57;
+    unsigned int Y = 57;
+
+    cout << "The NOT of X is: " << (~X) << "\n";
+    cout << "The NOT of Y is: " << (~Y) << "\n";
+}
