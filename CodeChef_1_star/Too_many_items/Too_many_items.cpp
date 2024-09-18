88f02ee55886a7936da93036be5b43ae54a4bee3
Arshvir <avarshvir@gmail.com>
2024-09-18 12:45:13 +0530

    Create Too_many_items.cpp

---
+++ CodeChef_1_star/Too_many_items/Too_many_items.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    int total_bag = (N+9)/10;
+	    cout << total_bag << endl; 
+	}
+
+}
