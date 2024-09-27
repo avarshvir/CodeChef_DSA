80c73631c84805511e518daac5a5ed8d3ba59ca7
Arshvir <avarshvir@gmail.com>
2024-09-27 21:13:25 +0530

    Dracula_Eats.cpp

---
+++ CodeChef_1_star/Dracula_Eats/Dracula_Eats.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    int k = (N+5)/7;
+	    cout << k << endl;
+	}
+
+}
