4d1aa877f81d65aaeb76269f9e5085907d59d404
Arshvir <avarshvir@gmail.com>
2024-10-13 22:22:33 +0530

    Richie Rich

---
+++ CodeChef_2_star/Richie_Rich/Richie_Rich.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int A,B,X;
+	    cin >> A >> B >> X;
+	    int t1 = B-A;
+	    int t2 = t1/X;
+	    
+	    cout << t2 << endl;
+	}
+
+}
