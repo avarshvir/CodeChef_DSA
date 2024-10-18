4458ef65ed79adf17c9abc6f33d983658bbd0383
Arshvir <avarshvir@gmail.com>
2024-10-18 18:16:56 +0530

    Hoop Jump

---
+++ CodeChef_2_star/Hoop_Jump/Hoop_Jump.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+        int last_hoop = (N+1)/2;
+        cout << last_hoop << endl;
+    }
+}
