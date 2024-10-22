2f1bb87d0b1affb523cf3d04817bca644f9a7107
Arshvir <avarshvir@gmail.com>
2024-10-22 18:57:15 +0530

    Problem Category

---
+++ CodeChef_2_star/Problem_Category/Problem_Category.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int x;
+	    cin >> x;
+	    if(x >=1 && x < 100){
+	        cout << "Easy" << endl;
+	    }
+	    else if(x >= 100 && x < 200){
+	        cout << "Medium" << endl;
+	    }
+	    else if(x >= 200 && xa <= 300){
+	        cout << "Hard" << endl;
+	    }
+	}
+
+}
